02967856747e9da7a27299c1349b796c47764939
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-04-19 19:53:49 -0400

    homework

---
+++ test.js
@@ -0,0 +1,21 @@
+var myArray = [];
+for (var i = 1; i < 11; i++) {
+    myArray.push(i);
+}
+
+var myArray = [];
+var i = 10; 
+while(i > 0) {
+    myArray.push(i);
+    i--;
+}
+
+var myArray = [];
+for (var i = 11; i < 22; i+= 2) {
+    myArray.push(i);
+}
+
+var myArray = [];
+for (var i = 15; i > 0; i-= 2) {
+    myArray.push(i);
+}
